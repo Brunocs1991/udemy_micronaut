75b1c35e4f6f5fe6f66360ea66feacf6159d7a06
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-07-25 17:00:04 -0300

    dependency injection from constructor

--- src/main/java/brunocs/com/udemy/controller/HelloWordController.java
+++ src/main/java/brunocs/com/udemy/controller/HelloWordController.java
@@ -4,15 +4,17 @@ import brunocs.com.udemy.service.HelloWordService;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 @Controller("/hello")
 public class HelloWordController {
 
-    @Inject
-    private HelloWordService service;
+    private final HelloWordService service;
+
+    public HelloWordController(final HelloWordService service) {
+        this.service = service;
+    }
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWrold() {
