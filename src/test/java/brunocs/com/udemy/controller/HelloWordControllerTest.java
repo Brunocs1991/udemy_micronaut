bc608034abf8920ff6394dbc00e30e6651bfb248
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-07-25 17:45:49 -0300

    logger in tests

--- src/test/java/brunocs/com/udemy/controller/HelloWordControllerTest.java
+++ src/test/java/brunocs/com/udemy/controller/HelloWordControllerTest.java
@@ -6,6 +6,8 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
@@ -14,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @MicronautTest
 class HelloWordControllerTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWordControllerTest.class);
+
     @Inject
     @Client("/")
     HttpClient httpClient;
@@ -21,6 +25,7 @@ class HelloWordControllerTest {
     @Test
     void helloWorldEndpointresponseWithTextHelloWorld() {
         var response = httpClient.toBlocking().retrieve("/hello");
+        LOG.trace("Response: {}", response);
         assertEquals("Hello Micronaut!", response);
     }
 
