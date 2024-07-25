0eefa5015b309195abc6869d9830636b5547128d
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-07-25 17:31:14 -0300

    Micronaut Application Context

--- src/main/java/brunocs/com/udemy/Application.java
+++ src/main/java/brunocs/com/udemy/Application.java
@@ -1,10 +1,18 @@
 package brunocs.com.udemy;
 
+import brunocs.com.udemy.service.HelloMicronautService;
+import brunocs.com.udemy.service.HelloWordService;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        var context = Micronaut.run(Application.class, args);
+        LOG.info("Message from service: {}", context.getBean(HelloWordService.class).helloFromService());
+        LOG.info("Message from service: {}", context.getBean(HelloMicronautService.class).helloFromService());
     }
 }
